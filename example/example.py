cf45b20349be9387569aab8f7a0be1566c83d497
Giancarlo Lucca <giancarlo.lucca88@gmail.com>
2023-03-15 13:16:21 -0300

    RBF1

--- example/example.py
+++ example/example.py
@@ -10,23 +10,25 @@ from functions.membership import FuzzyCMeansMembership
 import pandas as pd
 import matplotlib.pyplot as plt
 
-sm = 33
+#sm = 33
+sm = [1, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33]
 min_fmics = 5
 max_fmics = 100
 thresh = 0.5
-#threshList = [0.05, 0.1, 0.25, 0.5, 0.65, 0.8, 0.9]
-threshList = [0.85]
-chunksize=1000
-color = {'1': 'Red', '2': 'Blue', 'nan': 'Gray'}
+threshList = [0.25, 0.5, 0.65, 0.8, 0.9]
+#threshList = [0.85]
+chunksize = 1000
+#chunksize = 4000
+color = {'1': 'Red', '2': 'Blue', '3': 'Green', 'nan': 'Gray'}
 figure = plt.figure()
 scatter = plt.scatter('x', 'y', s='radius', data={'x': [], 'y': [], 'radius': []})
 
 #new_row = {'Chunk':12, 'Purity':12, 'pCoefficient':12, 'pEntropy':12, 'XieBeni':12}
 #df2 = df.append(new_row, ignore_index=True)
 
-for simIDX in range (1, sm+1):
+for simIDX in sm:
     for threshIDX in threshList:
-        df = pd.DataFrame(columns = ['Chunk', 'Purity', 'pCoefficient', 'pEntropy', 'XieBeni','FukuyamaSugeno_1','FukuyamaSugeno_2'])
+        df = pd.DataFrame(columns = ['Chunk', 'Purity', 'pCoefficient', 'pEntropy', 'XieBeni','MPC','FukuyamaSugeno_1','FukuyamaSugeno_2'])
         summarizer = DFuzzStreamSummarizer(
             distance_function=EuclideanDistance.distance,
             merge_threshold = threshIDX,
@@ -42,10 +44,12 @@ for simIDX in range (1, sm+1):
 
         # Read files in chunks
         with pd.read_csv("https://raw.githubusercontent.com/CIG-UFSCar/DS_Datasets/master/Synthetic/Non-Stationary/Bench1_11k/Benchmark1_11000.csv",
+        #with pd.read_csv("https://raw.githubusercontent.com/CIG-UFSCar/DS_Datasets/master/Synthetic/Non-Stationary/RBF1_40k/RBF1_40000.csv",
                         dtype={"X1": float, "X2": float, "class": str},
                         chunksize = chunksize) as reader:       
             for chunk in reader:
                 print(f"Summarizing examples from {timestamp} to {timestamp + 999} -> sim {simIDX} and thrsh {threshIDX}")
+                #print(f"Summarizing examples from {timestamp} to {timestamp + 3999} -> sim {simIDX} and thrsh {threshIDX}")
                 fhand.write(f"Summarizing examples from {timestamp} to {timestamp + 999} -> sim {simIDX} and thrsh {threshIDX}\n")
                 for index, example in chunk.iterrows():
                     #Summarizing example
@@ -53,7 +57,7 @@ for simIDX in range (1, sm+1):
                     timestamp += 1
 
                 
-                new_row = pd.DataFrame([["["+str(timestamp)+" to "+str(timestamp + 999)+"]", summarizer.Purity(), summarizer.PartitionCoefficient(), summarizer.PartitionEntropy(), summarizer.XieBeni(), summarizer.FukuyamaSugeno_1(), summarizer.FukuyamaSugeno_2()]], columns=df.columns)
+                new_row = pd.DataFrame([["["+str(timestamp)+" to "+str(timestamp + 999)+"]", summarizer.Purity(), summarizer.PartitionCoefficient(), summarizer.PartitionEntropy(), summarizer.XieBeni(), summarizer.ModifiedPartitionCoefficient(), summarizer.FukuyamaSugeno_1(), summarizer.FukuyamaSugeno_2()]], columns=df.columns)
                 df = pd.concat([df, new_row], ignore_index=True)
                 
                 fhand.write("Total de Fmics = "+str(len(summarizer.summary())))
@@ -87,7 +91,7 @@ for simIDX in range (1, sm+1):
                 #plt.legend(["color blue", "color green"], loc ="lower right")
                 #plt.legend(["Purity"+str(summarizer.Purity()),"PartitionCoefficient"+str(summarizer.PartitionCoefficient()),"PartitionEntropy"+str(summarizer.PartitionEntropy()),"XieBeni"+str(summarizer.XieBeni()), "FukuyamaSugeno_1"+str(summarizer.FukuyamaSugeno_1()),"FukuyamaSugeno_2"+str(summarizer.FukuyamaSugeno_2())], bbox_to_anchor=(1.05, 1), loc='upper left', borderaxespad=0)
                 plt.figtext(.8, .8, "T = 4K")
-                side_text = plt.figtext(.91, .8, "Purity: "+str(round(summarizer.Purity(), 3))+"\nPartitionCoefficient: "+str(round(summarizer.PartitionCoefficient(), 3))+"\nPartitionEntropy: "+str(round(summarizer.PartitionEntropy(), 3))+"\nXieBeni: "+str(round(summarizer.XieBeni(),3))+"\nFukuyamaSugeno_1: "+str(round(summarizer.FukuyamaSugeno_1(),3))+"\nFukuyamaSugeno_2: "+str(round(summarizer.FukuyamaSugeno_2(),3)))
+                side_text = plt.figtext(.91, .8, "Purity: "+str(round(summarizer.Purity(), 3))+"\nPartitionCoefficient: "+str(round(summarizer.PartitionCoefficient(), 3))+"\nPartitionEntropy: "+str(round(summarizer.PartitionEntropy(), 3))+"\nXieBeni: "+str(round(summarizer.XieBeni(),3))+"\nMPC: "+str(round(summarizer.ModifiedPartitionCoefficient()))+"\nFukuyamaSugeno_1: "+str(round(summarizer.FukuyamaSugeno_1(),3))+"\nFukuyamaSugeno_2: "+str(round(summarizer.FukuyamaSugeno_2(),3)))
                 fig.subplots_adjust(top=1.0)
                 #plt.show()
                 fig.savefig("./Img/[Chunk "+str(timestamp - 1000)+" to "+str(timestamp - 1)+"] Sim("+str(simIDX)+")_Thresh("+str(threshIDX)+").png", bbox_extra_artists=(side_text,), bbox_inches='tight')
