5d83be2cf9d134e3192c161b740c8d7665549c84
asieriko <asieriko@gmail.com>
2023-11-08 16:39:11 +0100

    1)Implemented the WFCM for the offline step, 2)refactored internal metrics from d-fuzzstream.py to metrics.py

---
+++ functions/metrics.py
@@ -0,0 +1,186 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Oct 24 21:02:34 2023
+
+@author: asier
+"""
+
+import numpy as np
+
+
+def all_online_metrics(fmics, chunksize):
+    return {
+        "Purity": Purity(fmics),
+        "PartitionCoefficent": PartitionCoefficient(fmics, chunksize),
+        "PartitionEntropy": PartitionEntropy(fmics, chunksize),
+        "XieBeni": XieBeni(fmics, chunksize),
+        "ModifiedPartitionCoefficent": ModifiedPartitionCoefficient(fmics,
+                                                                    chunksize),
+        "FukuyamaSugeno_1": FukuyamaSugeno_1(fmics),
+        "FukuyamaSugeno_2": FukuyamaSugeno_2(fmics, chunksize)
+        }
+
+
+def FS(x, c, mu, alpha=1):
+    '''
+    Fuzzy Silhouette
+
+    Parameters
+    ----------
+    x : Array data points
+        DESCRIPTION.
+    c : Cluster's centroids
+        DESCRIPTION.
+    mu : Membership matrix
+        DESCRIPTION.
+    alpha : Fuzzy weighted coefficient
+        Default 1
+
+    Returns
+    -------
+    Float [0,1].
+
+    '''
+    w = [1 for _ in range(len(x))]
+    return WFS(x, c, w, mu, alpha)
+
+def WFS(x, c, w, mu, alpha=1):
+    '''
+    Weighted Fuzzy Silhouette
+
+    Parameters
+    ----------
+    x : Array data points
+        DESCRIPTION.
+    c : Cluster's centroids
+        DESCRIPTION.
+    w : Weigths of examples
+        DESCRIPTION.
+    mu : Membership matrix
+        DESCRIPTION.
+    alpha : Fuzzy weighted coefficient
+        Default 1
+
+    Returns
+    -------
+    Float [0,1].
+
+    '''
+    n, s = x.shape
+    nc = len(c)
+    dist = np.zeros([n, nc])
+    for i in range(nc):
+        dist[:, i] = np.sqrt(np.sum((x-c[i, :])**2, axis=1))  # Euclidean
+    labels = np.argmin(dist, axis=1)
+    NC = [sum(labels == i) for i in range(nc)]  # points per cluster
+    dm = np.zeros([n, n])
+    for i in range(n):
+        for j in range(i, n):
+            dm[i][j] = np.sqrt(sum((x[i] - x[j])**2))
+            dm[j][i] = dm[i][j]
+
+    s = []
+    for i in range(n):
+        # aij
+        aij = 0
+        cluster = labels[i]
+        same_cluster_idx = np.argwhere(labels == cluster)[:, 0]
+        for j in same_cluster_idx:
+            if i != j:
+                aij += dm[i, j]
+        aij = aij/(NC[cluster]-1)
+
+        # bij
+        bij = []
+        for p in range(nc):
+            if p != cluster:
+                bij.append(0)
+                other_cluster_idx = np.argwhere(labels == p)[:, 0]
+                for j in other_cluster_idx:
+                    bij[-1] += dm[i, j]
+                bij[-1] = bij[-1]/NC[p]
+        bij = min(bij)
+
+        si = (bij - aij)/max(aij, bij)
+        s.append(si)
+
+    # https://www.w3resource.com/python-exercises/numpy/advanced-numpy-exercise-11.php
+    second_largest = np.partition(mu, -2, axis=0)
+    muijp = second_largest[-1] - second_largest[-2]
+
+    wfs = sum((muijp**alpha)*s*w)/sum((muijp**alpha)*w)
+    return wfs
+
+def Purity(fmics):
+    majorityClass = 0
+    totalPoints = 0
+    for idxFMIC, fmic in enumerate(fmics):
+        majorityClass += np.max(fmic.sumPointsPerClass)
+        totalPoints += np.sum(fmic.sumPointsPerClass)
+
+    return (1/totalPoints * majorityClass)
+
+def PartitionCoefficient(fmics, chunksize):
+    mSquare = 0
+    for idxFMIC, fmic in enumerate(fmics):
+        mSquare += fmic.mSquare
+
+    return (1/chunksize * mSquare)
+
+def ModifiedPartitionCoefficient(fmics, chunksize):
+    mSquare = 0
+    for idxFMIC, fmic in enumerate(fmics):
+        mSquare += fmic.mSquare
+
+    return 1 - ((len(fmics)/len(fmics)-1) * (1 - (1/chunksize * mSquare)))
+
+def PartitionEntropy(fmics, chunksize):
+    mLog = 0
+    for idxFMIC, fmic in enumerate(fmics):
+        mLog += fmic.mLog
+
+    return (- 1/chunksize * mLog)
+
+def XieBeni(fmics, chunksize):
+    sumaSSD = 0
+    centroidList = np.ones((len(fmics), 2))*1000000
+    menorDistancia = 1000000
+    # storing the distances among all Fmics
+    for idxFMIC, fmic in enumerate(fmics):
+        sumaSSD += fmic.ssd
+        centroidList[idxFMIC, :] = fmic.center
+
+    MinDist = np.min(np.linalg.norm(centroidList, axis=1))
+
+    return (1/chunksize * sumaSSD)/MinDist
+
+def FukuyamaSugeno_1(fmics):
+    sumaSSD = 0
+    centroidList = np.ones((len(fmics), 2))
+    membershipList = np.ones(len(fmics))
+
+    for idxFMIC, fmic in enumerate(fmics):
+        sumaSSD += fmic.ssd
+        centroidList[idxFMIC, :] = fmic.center
+        membershipList[idxFMIC] = fmic.m
+
+    V1 = np.sum(centroidList/len(fmics), axis=0)
+
+    return sumaSSD - np.sum(membershipList * np.linalg.norm(centroidList - V1, axis=1))
+
+def FukuyamaSugeno_2(fmics, chunksize):
+    sumaSSD = 0
+    sumaValues = 0
+    centroidList = np.ones((len(fmics), 2))
+    membershipList = np.ones(len(fmics))
+
+    for idxFMIC, fmic in enumerate(fmics):
+        sumaSSD += fmic.ssd
+        centroidList[idxFMIC, :] = fmic.center
+        membershipList[idxFMIC] = fmic.m
+        sumaValues += 1/chunksize * fmic.values
+
+    V2 = sumaValues
+
+    return sumaSSD - np.sum(membershipList * np.linalg.norm(centroidList - V2, axis=1))
