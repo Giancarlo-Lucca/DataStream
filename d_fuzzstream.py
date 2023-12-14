e86fe844a65ed20ebd06f472a215a039ec6c1644
asieriko <asieriko@gmail.com>
2023-12-14 10:15:19 +0100

    Removed metrics from main stream class to metrics file, fixed purity on metrics.py, some cleanup and formating, updated example.py

--- d_fuzzstream.py
+++ d_fuzzstream.py
@@ -4,7 +4,7 @@ from functions import membership
 from functions import merge
 from functions.WFCM import WFCM  # Asier: WFCM for offline step
 import numpy as np
-import pandas as pd  # Asier
+
 
 class DFuzzStreamSummarizer:
 
@@ -20,8 +20,8 @@ class DFuzzStreamSummarizer:
             # merge_function=merge.FuzzyDissimilarityMerger.merge
             merge_function=merge.FuzzyDissimilarityMerger(1, 100).merge,
             chunksize=1000,
-            n_macro_clusters=20, # Asier: number of cluster for the WFCM
-            time_gap=10000, # Asier: When to apply the WFCM
+            n_macro_clusters=20,  # Asier: number of cluster for the WFCM
+            time_gap=1000,  # Asier: When to apply the WFCM
     ):
         self.min_fmics = min_fmics
         self.max_fmics = max_fmics
@@ -35,8 +35,8 @@ class DFuzzStreamSummarizer:
         self.__merge_function = merge_function
         self.metrics = {'creations': 0, 'absorptions': 0, 'removals': 0, 'merges': 0}
         self.chunksize = chunksize
-        self.n_macro_clusters=n_macro_clusters # Asier
-        self.time_gap = time_gap # Asier
+        self.n_macro_clusters = n_macro_clusters  # Asier
+        self.time_gap = time_gap  # Asier
         self._V = []   # Asier: Centroids of the offline step
         self._Vmm = []  # Asier: Membership matrix of the online step
 
@@ -44,6 +44,14 @@ class DFuzzStreamSummarizer:
         if len(self.__fmics) < self.min_fmics:
             self.__fmics.append(FMiC(values, tag, timestamp))
             self.metrics['creations'] += 1
+
+            # Asier: Update the actual clusters. It seems that in some cases
+            # can happen that there is only a few mic and a new is created
+            # on the offline evaluation timestamp, so it was not called due
+            # to the return statement. So I add here the code
+            if (timestamp + 1) % self.time_gap == 0:
+                self.offline()
+
             return
 
         distance_from_fmics = [self.__distance_function(fmic.center, values) for fmic in self.__fmics]
@@ -83,13 +91,14 @@ class DFuzzStreamSummarizer:
         self.metrics['merges'] += number_of_fmics - len(self.__fmics)
 
         # Asier: Update the actual clusters
-        if timestamp % self.time_gap == 0:
+        if (timestamp + 1) % self.time_gap == 0:
             self.offline()
 
     # Asier
     def offline(self):
         data = np.array([fm.center.to_list() for fm in self.__fmics])
         w = [fm.m for fm in self.__fmics]  # Sum of membership
+        f = [fm for fm in self.__fmics]
         self._V, self._Vmm = WFCM(data, w, c=self.n_macro_clusters)
 
     def final_clustering(self):
@@ -97,81 +106,6 @@ class DFuzzStreamSummarizer:
             self.offline()
 
         return self._V, self._Vmm
-
-
-    # Asier: Metrics better in functions.metrics
-    # def Purity(self):
-    #     majorityClass = 0
-    #     totalPoints = 0
-    #     for idxFMIC, fmic in enumerate(self.__fmics):
-    #         majorityClass += np.max(fmic.sumPointsPerClass)
-    #         totalPoints += np.sum(fmic.sumPointsPerClass)
-
-    #     return (1/totalPoints * majorityClass)
-
-    # def PartitionCoefficient(self):
-    #     mSquare = 0
-    #     for idxFMIC, fmic in enumerate(self.__fmics):
-    #         mSquare += fmic.mSquare
-
-    #     return (1/self.chunksize * mSquare)
-
-    # def ModifiedPartitionCoefficient(self):
-    #     mSquare = 0
-    #     for idxFMIC, fmic in enumerate(self.__fmics):
-    #         mSquare += fmic.mSquare
-
-    #     return 1 - ((len(self.__fmics)/len(self.__fmics)-1) * (1 - (1/self.chunksize * mSquare)))
-
-    # def PartitionEntropy(self):
-    #     mLog = 0
-    #     for idxFMIC, fmic in enumerate(self.__fmics):
-    #         mLog += fmic.mLog
-
-    #     return (- 1/self.chunksize * mLog)
-
-    # def XieBeni(self):
-    #     sumaSSD = 0
-    #     centroidList = np.ones((len(self.__fmics), 2))*1000000
-    #     menorDistancia = 1000000
-    #     # storing the distances among all Fmics
-    #     for idxFMIC, fmic in enumerate(self.__fmics):
-    #         sumaSSD += fmic.ssd
-    #         centroidList[idxFMIC, :] = fmic.center
-
-    #     MinDist = np.min(np.linalg.norm(centroidList, axis=1))
-
-    #     return (1/self.chunksize * sumaSSD)/MinDist
-
-    # def FukuyamaSugeno_1(self):
-    #     sumaSSD = 0
-    #     centroidList = np.ones((len(self.__fmics), 2))
-    #     membershipList = np.ones(len(self.__fmics))
-
-    #     for idxFMIC, fmic in enumerate(self.__fmics):
-    #         sumaSSD += fmic.ssd
-    #         centroidList[idxFMIC, :] = fmic.center
-    #         membershipList[idxFMIC] = fmic.m
-
-    #     V1 = np.sum(centroidList/len(self.__fmics), axis=0)
-
-    #     return sumaSSD - np.sum(membershipList * np.linalg.norm(centroidList - V1, axis=1))
-
-    # def FukuyamaSugeno_2(self):
-    #     sumaSSD = 0
-    #     sumaValues = 0
-    #     centroidList = np.ones((len(self.__fmics), 2))
-    #     membershipList = np.ones(len(self.__fmics))
-
-    #     for idxFMIC, fmic in enumerate(self.__fmics):
-    #         sumaSSD += fmic.ssd
-    #         centroidList[idxFMIC, :] = fmic.center
-    #         membershipList[idxFMIC] = fmic.m
-    #         sumaValues += 1/self.chunksize * fmic.values
-
-    #     V2 = sumaValues
-
-    #     return sumaSSD - np.sum(membershipList * np.linalg.norm(centroidList - V2, axis=1))
 
     def summary(self):
         return self.__fmics.copy()
